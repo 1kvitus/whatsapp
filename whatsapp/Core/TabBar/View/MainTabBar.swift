ebda5fe50b1999797293c881818521451e2e0b7f
Kalash <kalash.choudhary@sekai-ichi.com>
2024-05-04 23:01:30 +0530

    MainTabBar Created

---
+++ whatsapp/Core/TabBar/View/MainTabBar.swift
@@ -0,0 +1,68 @@
+//
+//  MainTabBar.swift
+//  whatsapp
+//
+//  Created by choudhary kalash on 04/05/24.
+//
+
+import SwiftUI
+
+struct MainTabBar: View {
+    
+  @State private var selectTab: Int = 0
+    var body: some View {
+        TabView {
+            
+            Text("Inbox")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "bubble.fill")
+                            .environment(\.symbolVariants, selectTab == 0 ? .fill : .none)
+                        Text("Chat")
+                    }
+                    .onAppear() {
+                        selectTab = 0
+                    }
+                }
+            Text("Updates")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "dial.low")
+                            .environment(\.symbolVariants, selectTab == 0 ? .fill : .none)
+                        Text("Updates")
+                    }
+                    .onAppear() {
+                        selectTab = 0
+                    }
+                }
+        
+            Text("Communities")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "person.3")
+                            .environment(\.symbolVariants, selectTab == 0 ? .fill : .none)
+                        Text("Communities")
+                    }
+                    .onAppear() {
+                        selectTab = 0
+                    }
+                }
+            Text("Calls")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "phone")
+                            .environment(\.symbolVariants, selectTab == 1 ? .fill : .none)
+                        Text("Calls")
+                    }
+                    .onAppear() {
+                        selectTab = 0
+                    }
+                }
+            
+        }
+    }
+}
+
+#Preview {
+    MainTabBar()
+}
