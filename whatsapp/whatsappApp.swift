0da3cbed185a506abaec639e944b0e8f85407ade
Kalash <kalash.choudhary@sekai-ichi.com>
2024-05-04 20:08:58 +0530

    Initial Commit

---
+++ whatsapp/whatsappApp.swift
@@ -0,0 +1,17 @@
+//
+//  whatsappApp.swift
+//  whatsapp
+//
+//  Created by choudhary kalash on 04/05/24.
+//
+
+import SwiftUI
+
+@main
+struct whatsappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
